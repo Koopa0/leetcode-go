719c69b9bea34ddb31d642000a85fd6e64774098
Koopa <koopapapa@gmail.com>
2025-03-28 10:55:18 +0800

    refactor maxArea function for clarity; implement dual-pointer technique and update README with problem explanation and solution analysis

--- leetcode/ContainerWithMostWater/main.go
+++ leetcode/ContainerWithMostWater/main.go
@@ -1,17 +1,24 @@
 package ContainerWithMostWater
 
 func maxArea(height []int) int {
+	// 初始化變量
+	maxWater := 0
+	left := 0
+	right := len(height) - 1
 
-	left, right := 0, len(height)-1
-
-	maxArea := 0
-
+	// 使用雙指針技術
 	for left < right {
-		area := (right - left) * min(height[left], height[right])
-		if area > maxArea {
-			maxArea = area
-		}
+		// 計算當前容器的寬度
+		width := right - left
 
+		// 容器的高度由較短的線決定
+		h := min(height[left], height[right])
+
+		// 計算當前容器的容量並更新最大容量
+		currentArea := width * h
+		maxWater = max(maxWater, currentArea)
+
+		// 移動指向較短線的指針
 		if height[left] < height[right] {
 			left++
 		} else {
@@ -19,5 +26,5 @@ func maxArea(height []int) int {
 		}
 	}
 
-	return maxArea
+	return maxWater
 }
